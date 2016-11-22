ef77c362dd9734ab858269e484e7188d820129b2
Yu0972 <105021045@live.asia.edu.tw>
2016-11-22 16:40:24 +0800

    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,15 +1,22 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 請寫一個函式 (不是遞迴) 進行十進制數字轉換成二進制數字，例如十進制的 13 會輸出二進制的 1101
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021045 郭毓呈
  */
 
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner ( System.in ) ;
+		int n = scn.nextInt ( ) ;
+		String sum = " " ;
+		while ( n > 0 ) {
+			sum = n % 2 + sum ;
+			n = n / 2 ;
+}
+		System.out.print ( sum ) ;
 	}
-
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,30 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 將前一題加強成可以做10以下的各種數字系統轉換
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021045 郭毓呈
  */
 
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner ( System.in ) ;
+		int n = scn.nextInt ( ) ;
+		int m = scn.nextInt ( ) ;
+		System.out.print ( fun ( n , m ) ) ;
+}
+	public static int fun ( int x , int m ) {
+		int sum = 0 ;
+		int sum2 = 0 ;
+		int y = 1 ;	
+		while ( x > 0 ) {
+		sum = x % m ;
+		x = x / m ;
+		sum2 = sum2 + ( sum * y ) ;
+		y = y * 10 ;
+}
+		return sum2 ;
 	}
-
 }
